b4cae33896a8c7cff9a208cec329b0b739e00afd
mehmehmehlol <hkmeganlo@gmail.com>
2021-05-07 17:28:51 -0700

    added developer's name on top of the files

--- src/Explorers/Explorers.js
+++ src/Explorers/Explorers.js
@@ -2,10 +2,12 @@ import React from 'react'
 import './Explorers.css'
 
 const Explorers = () => {
+  // Alyssa
   return (
     <div>
       <h1>How does it work for explorers?</h1>
       <h3>1 Professionals have career path experience that students want.</h3>
+      {/* import guide recommendation */}
     </div>
   )
 }
