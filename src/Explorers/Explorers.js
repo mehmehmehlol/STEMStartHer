d7c48c2af72907de83defca0da54e61dd2b318f7
Alyssa <alymaebenipayo@gmail.com>
2021-05-09 22:28:39 -0700

    reverted to previous changes to resolve git issue

--- src/Explorers/Explorers.js
+++ src/Explorers/Explorers.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import './Explorers.css'
+<<<<<<< HEAD
 import img from './images/Section-Bubbles.svg'
 import Carousel from 'react-elastic-carousel'
 import Item from './Item'
 import { Link } from 'react-router-dom'
+=======
+import './images/Section-Bubbles.svg'
+>>>>>>> parent of 96bd043 (Updated with header bar and section bubbles)
 
 const Explorers = ({ guides }) => {
   return (
-    <div className="header">
-      <div className="header-bar"></div>
+    <div class="header">
       <h1>How does it work for explorers?</h1>
-      <img className="section-bubbles" src={img}/>
       {/* import guide recommendation */}
       <h3>1 Professionals have career path experience that students want.</h3>
       <>
@@ -38,5 +40,9 @@ const Explorers = ({ guides }) => {
     </div>
   )
 }
+
+var myImage = new Image(1280, 1840);
+myImage.src = 'section-bubbles.svg';
+document.body.appendChild(myImage);
 
 export default Explorers
