91712935481d4ec086a63ad19be8464b3b4fce9b
Diane <dianegeroy@gmail.com>
2021-05-11 19:39:49 +0800

    updated css for guide cards and text styling for a links

--- src/RandomGuidesGenerator.js
+++ src/RandomGuidesGenerator.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+
 const RandomGuidesGenerator = ({ guides }) => {
   // const randFour = () => {
   //   const random = [];
@@ -15,6 +16,7 @@ const RandomGuidesGenerator = ({ guides }) => {
 
   return (
     // <div className="p-4 m-4">
+      
       <div className="container-fluid guide-cards row mb-4 p-4">
         {
           guides.slice(0, 4).map((guide) => (
@@ -22,8 +24,8 @@ const RandomGuidesGenerator = ({ guides }) => {
             // <div key={ guide.id }>
               <div key={ guide.id }>
                 <div className="col md-3">
-                  <div className="card" style={{ height: 300, width: 300 }}>
-                    <img className="card-img-top" src={ guide.profile_pic } alt={ guide.id } style={{ height: 400, width: 295}}/>
+                  <div className="card" style={{ width: 300 }}>
+                    <img className="card-img-top" src={ guide.profile_pic } alt={ guide.id } style={{ height: 400}}/>
                     <div className="card-body">
                       <h4 classname="card-title"><Link to={`/guides/${guide.first_name}`}>{guide.first_name} {guide.last_name}</Link></h4>
                       <h5 className="card-text">{ guide.job_title }</h5>
@@ -39,6 +41,7 @@ const RandomGuidesGenerator = ({ guides }) => {
           ))
         }
       </div>
+      
     // </div>
   )
 }
