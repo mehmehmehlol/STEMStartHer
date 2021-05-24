956e9620da6c9be0ef974706cf73af5a80124053
mehmehmehlol <hkmeganlo@gmail.com>
2021-05-24 13:33:53 -0700

    fixed slow loading image problem

--- src/Explorers/Explorers.js
+++ src/Explorers/Explorers.js
@@ -5,7 +5,10 @@ import Item from './Item'
 import { Link } from 'react-router-dom'
 import img0 from '../images/Section-Bubbles.svg'
 import img1 from '../images/Companies.svg'
-import img2 from '../images/Testimonial.svg'
+// import img2 from '../images/Testimonial.svg'
+import Explorer1 from '../images/explorer-1.svg'
+import Explorer2 from '../images/explorer-2.svg'
+import Explorer3 from '../images/explorer-3.svg'
 
 const Explorers = ({ guides }) => {
   return (
@@ -16,12 +19,12 @@ const Explorers = ({ guides }) => {
       <img className="section-bubble" alt="Section Bubble" src={img0}/>
         
       <>
-        <h4 className="heading">Check out our Guides!</h4>
+        <h1 className="heading">Check out our explorers!</h1>
         <Carousel itemsToShow={4} itemsToScroll={2}>
           { guides.map(guide => (
             <Item key={ guide.id }>
-              <img className="guide-image" src={guide.profile_pic} alt={guide.first_name} />
-              <h4>Name: <Link to={`/guides/${guide.first_name}`}>{guide.first_name} {guide.last_name}</Link></h4>
+              <img className="explorer-image" src={guide.profile_pic} alt={guide.first_name} />
+              <h4>Name: <Link to={`/explorers/${guide.first_name}`}>{guide.first_name} {guide.last_name}</Link></h4>
               {/* image work logo */}
               <h4>{guide.job_title}</h4>
               <h4>{guide.company}</h4>
@@ -34,13 +37,35 @@ const Explorers = ({ guides }) => {
       <>
       <img className="companies" alt="Companies" src={img1}/>
       </>
-      <img className="testimonial" alt="Testimonial" src={img2}/>
-      <>
+      <div className="explorer-testimonial">
+        <h1>Testimonials from past explorers</h1>
+        <div className="explorer-testimonial-content row">
+            <div className="col-md-3">
+              <img src={Explorer1} alt="Explorer 1 Pic" />
+            </div>
+            <div className="col-md-9">
+              <p>“I used to be scared of networking, but ExplorHer pushed me out of my comfort zone and got rid of that fear. It actually helped me build confidence, connected me with women who encouraged me to hone my skills, and paved new paths for me in the STEM fields.”</p>
+            </div>
+
+            <div className="col-md-3">
+              <img src={Explorer2} alt="Explorer 2 Pic" />
+            </div>
+            <div className="col-md-9">
+              <p>“I used to be scared of networking, but ExplorHer pushed me out of my comfort zone and got rid of that fear. It actually helped me build confidence, connected me with women who encouraged me to hone my skills, and paved new paths for me in the STEM fields.”</p>
+          </div>
+
+          <div className="col-md-3">
+            <img src={Explorer3} alt="Explorer 3 Pic" />
+          </div>
+          <div className="col-md-9">
+            <p>“I used to be scared of networking, but ExplorHer pushed me out of my comfort zone and got rid of that fear. It actually helped me build confidence, connected me with women who encouraged me to hone my skills, and paved new paths for me in the STEM fields.”</p>
+          </div>      
+        </div>
+      </div>
       <div className="sign-up">
         <h5>Ready to become an Explorer?</h5>
         <button className="sign-up-btn button blue"><Link to="/signup" style={{ color: "black" }}>Sign Up</Link></button>
         </div>
-      </>
     </div>
   )
 }
